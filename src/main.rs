67a5ce52dd66761ddf90131294622728a49b613b
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-14 14:09:59 +0700

    feat(bencoded): handle decode lists

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,79 @@
-use std::env;
+use std::{env, iter::Peekable, str::Chars};
 
-use serde_json::Number;
+use serde::{Deserialize, Serialize};
 
 // Available if you need it!
 // use serde_bencode
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum Bencode {
+    String(String),
+    Integer(i64),
+    List(Vec<Bencode>),
+}
 
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    let err_msg = format!("Unhandled encoded value: {}", encoded_value);
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_ascii_digit() {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').expect(&err_msg);
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().expect(&err_msg);
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        serde_json::Value::String(string.to_string())
-    } else if encoded_value.starts_with('i') && encoded_value.ends_with('e') {
-        let number_string = &encoded_value[1..encoded_value.len() - 1];
-        let number = number_string.parse::<i64>().expect(&err_msg);
-        serde_json::Value::Number(Number::from(number))
-    } else {
-        panic!("{}", err_msg)
+fn decode_bencoded_value(chars: &mut Peekable<Chars>) -> Result<Bencode, String> {
+    let err_msg = format!("Unhandled encoded value: {}", chars.clone().collect::<String>());
+    match chars.next() {
+        Some(x) if x.is_ascii_digit() => {
+            // Example: "5:hello" -> "hello"
+            let mut number_token = x.to_string();
+            let mut curr = chars.next();
+            loop {
+                match curr {
+                    Some(':') => break,
+                    Some(c) => number_token.push(c),
+                    None => return Err(err_msg),
+                }
+                curr = chars.next();
+            }
+            let number = match number_token.parse::<i64>() {
+                Ok(it) => it,
+                Err(_) => return Err(err_msg),
+            };
+            let mut txt = String::new();
+            for _ in 0..number {
+                match chars.next() {
+                    Some(c) => txt.push(c),
+                    None => return Err(err_msg),
+                }
+            }
+            Ok(Bencode::String(txt))
+        }
+        Some('i') => {
+            // Example: "i52e" -> 52
+            let mut number_string = String::new();
+            let mut curr = chars.next();
+            loop {
+                match curr {
+                    Some('e') => break,
+                    Some(c) => number_string.push(c),
+                    None => return Err(err_msg),
+                }
+                curr = chars.next();
+            }
+            let number = match number_string.parse::<i64>() {
+                Ok(it) => it,
+                Err(_) => return Err(err_msg),
+            };
+            Ok(Bencode::Integer(number))
+        }
+        Some('l') => {
+            // Example: "l5:helloi52ee" -> ["hello",52]
+            let mut rt: Vec<Bencode> = Vec::new();
+            loop {
+                if let Some('e') = chars.peek() {
+                    chars.next();
+                    break;
+                }
+                match decode_bencoded_value(chars) {
+                    Ok(x) => rt.push(x),
+                    Err(_) => return Err(err_msg),
+                }
+            }
+            Ok(Bencode::List(rt))
+        }
+        _ => Err(err_msg),
     }
 }
 
@@ -31,8 +84,12 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value);
+        match decode_bencoded_value(&mut encoded_value.chars().peekable()) {
+            Ok(decoded_value) => {
+                println!("{}", serde_json::to_value(decoded_value).unwrap());
+            }
+            Err(err) => panic!("{}", err),
+        };
     } else {
         println!("unknown command: {}", args[1])
     }
